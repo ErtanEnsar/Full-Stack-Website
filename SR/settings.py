8f6cc44d2425b0fe47c9d813adf92de5557f8566
ErtanEnsar <erta0015@live.dk>
2021-07-31 01:47:15 +0300

    package

--- SR/settings.py
+++ SR/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import gunicorn
 import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
