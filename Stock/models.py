3cd93adf6990a5722ddf3a9c3089db166839f8f4
ErtanEnsar <erta0015@live.dk>
2021-07-31 22:49:22 +0300

    Home, Nav, Footer, Search & Models

---
+++ Stock/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Stocks(models.Model):
+    Name = models.CharField(max_length=45)
+    Ticker = models.CharField(max_length=20)
+    Exchange = models.CharField(max_length=150)
+    Sentiment_Today = models.IntegerField(blank=True, null=True)
+    Mentions_Today = models.IntegerField(blank=True, null=True)
