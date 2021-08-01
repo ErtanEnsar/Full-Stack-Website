2360a3b20c2304eb852106a886e779484dc64a5f
ErtanEnsar <erta0015@live.dk>
2021-08-02 02:03:47 +0300

    One to Many Stock model

--- Stock/models.py
+++ Stock/models.py
@@ -9,7 +9,6 @@ from datetime import date
 #     Mentions_Today = models.IntegerField(blank=True, null=True)
 #
 #
-
 # class Stock(models.Model):
 #     Name = models.CharField(max_length=45)
 #     Ticker = models.CharField(max_length=20)
