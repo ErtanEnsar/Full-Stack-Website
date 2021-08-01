57c1e30ed226a7034bba216a3efe367a1731ca92
ErtanEnsar <erta0015@live.dk>
2021-08-02 00:16:02 +0300

    One to Many Stock model

--- Stock/models.py
+++ Stock/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+class Date(models.Model):
+    Date_Today = models.DateField(default=date.today)
+    Sentiment_Today = models.IntegerField(blank=True, null=True)
+    Mentions_Today = models.IntegerField(blank=True, null=True)
+
+
 class Stock(models.Model):
     Name = models.CharField(max_length=45)
     Ticker = models.CharField(max_length=20)
     Exchange = models.CharField(max_length=150)
-    Sentiment_Today = models.IntegerField(blank=True, null=True)
-    Mentions_Today = models.IntegerField(blank=True, null=True)
+    Date_Today = models.ForeignKey(Date, on_delete=models.PROTECT)
+
