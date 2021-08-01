9ea326e73304f9751b0fdd7f1ca810c290dd2d43
ErtanEnsar <erta0015@live.dk>
2021-08-02 00:37:32 +0300

    One to Many Stock model

--- Stock/admin.py
+++ Stock/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Stock
 from .models import Date
-admin.site.register(Stock,Date)
+admin.site.register(Stock)
+admin.site.register(Date)
