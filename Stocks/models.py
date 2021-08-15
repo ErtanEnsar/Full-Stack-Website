b72f82b9d97874c67770de505e3ada09e0e7a8c6
ErtanEnsar <erta0015@live.dk>
2021-08-16 02:26:24 +0300

    changes model

--- Stocks/models.py
+++ Stocks/models.py
@@ -7,7 +7,7 @@ class Stock(models.Model):
     Ticker = models.CharField(max_length=20)
     Exchange = models.CharField(max_length=150)
 
-class TodaysData(models.Model):
+class Date(models.Model):
     Stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
     Date_Today = models.DateField(default=date.today)
     Sentiment_Today = models.IntegerField(blank=True, null=True)
