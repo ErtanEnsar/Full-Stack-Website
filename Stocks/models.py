00cea8129a0ea014f5f57c60f38737a0dc0e9867
ErtanEnsar <erta0015@live.dk>
2021-08-16 13:37:56 +0300

    new model

--- Stocks/models.py
+++ Stocks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import date
+
+
 # Create your models here.
 
 class Stock(models.Model):
@@ -7,9 +9,9 @@ class Stock(models.Model):
     Ticker = models.CharField(max_length=20)
     Exchange = models.CharField(max_length=150)
 
+
 class StockData(models.Model):
     Stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
     Date_Today = models.DateField(default=date.today)
     Sentiment_Today = models.IntegerField(blank=True, null=True)
     Mentions_Today = models.IntegerField(blank=True, null=True)
-    Price = models.IntegerField(blank=True, null=True)
